93aa30eae691ab6f0e3727d7b62e1e9ce25538f0
Tuana <tuanaaunall10@gmail.com>
2026-07-17 14:56:22 +0300

    İlk kurulum: Proje yapısı, gereksinimler ve git bağlantısı tamamlandı

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+araclarin takibi: yolo kullanalim, training yapmayalim zaten yolo default olarak araclarin tespitini yapabiliyor.
+
+"""
+
+from ultralytics import YOLO
+import cv2
+
+# veri seti incele
+
+# yolo modeli yukle
+
+# tracking algoritmasi ve testing
